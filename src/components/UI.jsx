fc47f044ee15e2918a07e4027b62aec47d16be44
Joshua Garner <jgarner1989@yahoo.com>
2021-03-17 21:30:12 -0400

    started colors and onchanges

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,19 +1,51 @@
+import { useState } from 'react';
+
+
 const UI = () => {
+
+    const [zip, setZip] = useState('');
+    const [weather, setWeather] = useState('');
+    const [tcolor, setTcolor] = useState('');
+    
+
+    const _handleEntry = (e) => {
+        setZip(e.target.value);
+    };
+
+
+    const _handleSubmit = async (e) => {
+            e.preventDefault();
+            const weatherData = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=f07b010bc0b89ba24ca495e6020128d3`
+            ).then((response) => response.json());
+            setWeather(weatherData);
+            // console.log(weatherData);
+            }
+            // console.log(weather)
+
+    
+    const _style = (e) =>{
+        const color = e.target.value
+        setTcolor(color)
+    }
+            
+
     return (
         <div>
             <>
-            <form>
-                <label for='zip'> What Would You Like To See?
+            <form onSubmit={_handleSubmit}>
+                <label htmlFor='zip'> What Would You Like To See?
                     </label><br/>
                     <input
                         type='text'
                         id='zip'
                         name='zip'
                         placeholder = "Enter Your Zip:"
+                        onChange={_handleEntry}
                     /> <br/>   
-                     <label for='tcolor'> Choose A Text Color &nbsp;
+                     <label htmlFor='tcolor'> Choose A Text Color &nbsp;
                     </label>
-                    <select name="tcolor" id="tcolor">
+                    <select name="tcolor" id="tcolor" style={{backgroundColor: tcolor}} onChange={_style}>
                         <option value="green">Green</option>
                         <option value="red">Red</option>
                         <option value="black">Black</option>
@@ -21,7 +53,7 @@ const UI = () => {
                         <option value="Purple">Purple</option>
                         <option value="yellow">Yellow</option>
                     </select><br/>
-                    <label for='bcolor'> Choose A Background Color &nbsp;
+                    <label htmlFor='bcolor'> Choose A Background Color &nbsp;
                     </label>
                     <select name="bcolor" id="bcolor">
                     <option value="black">Black</option>
@@ -31,7 +63,7 @@ const UI = () => {
                         <option value="Purple">Purple</option>
                         <option value="yellow">Yellow</option>
                     </select><br/>
-                    <label for='tmethod'> Choose A Method Of Transportation &nbsp;
+                    <label htmlFor='tmethod'> Choose A Method Of Transportation &nbsp;
                     </label>
                     <select name="tmethod" id="tmethod">
                         <optgroup label = 'Private'>
@@ -51,7 +83,7 @@ const UI = () => {
                 
                 <button type='submit'>See Weather</button>
             </form>
-            
+            {/* {!!submitError && <div className='error'>{submitError}</div>} */}
         </>
         </div>
     )
---
+++ src/components/Weather7.jsx
@@ -0,0 +1,11 @@
+
+
+const Weather7 = ({weather}) => {
+    return (
+        <div>
+            <h1>{weather}</h1>
+        </div>
+    )
+}
+
+export default Weather7
